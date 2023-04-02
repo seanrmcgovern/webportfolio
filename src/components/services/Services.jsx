58c8bd6f1c7042687fed7139bdcb9ae75d51b27d
Sean McGovern <srm5ka@virginia.edu>
2023-04-02 14:16:32 -0400

    Added services section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,97 @@
 import React from 'react'
 import './services.css'
+import { HiCheck } from "react-icons/hi"
 
 const Services = () => {
   return (
-    <section id="services">Services</section>
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className="container services-container">
+        <article className="service">
+          <div className="service-head">
+            <h3>Problem Solving</h3>
+          </div>
+
+          <ul className="service-list">
+            <li>
+              <HiCheck className="service-list-icon"/>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <HiCheck className="service-list-icon"/>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <HiCheck className="service-list-icon"/>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className="service">
+          <div className="service-head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className="service-list">
+            <li>
+              <HiCheck className="service-list-icon"/>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <HiCheck className="service-list-icon"/>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <HiCheck className="service-list-icon"/>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <HiCheck className="service-list-icon"/>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <HiCheck className="service-list-icon"/>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <HiCheck className="service-list-icon"/>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className="service">
+          <div className="service-head">
+            <h3>Communication + Planning</h3>
+          </div>
+
+          <ul className="service-list">
+            <li>
+              <HiCheck className="service-list-icon"/>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <HiCheck className="service-list-icon"/>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <HiCheck className="service-list-icon"/>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <HiCheck className="service-list-icon"/>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <HiCheck className="service-list-icon"/>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
   )
 }
 
