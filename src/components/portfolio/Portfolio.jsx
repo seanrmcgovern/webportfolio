573dfa1283e66f53c6fad375052f7c8c5e38db83
Sean McGovern <srm5ka@virginia.edu>
2023-04-02 15:48:55 -0400

    Added portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './portfolio.css'
+import { Projects } from "./Projects";
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio-container">
+      {Projects.map(({id, title, github, demo, image}) => (
+          <article className="portfolio-item" key={id}>
+            <div className="portfolio-item-image">
+              <img src={image} alt={title} className="portfolio-image"></img>
+            </div>
+            <h3>{title}</h3>
+            <div className="portfolio-item-cta">
+              <a href={github} className="btn" target="_blank" rel="noreferrer">Github</a>
+              <a href={demo} className="btn btn-primary" target="_blank" rel="noreferrer">Video Demo</a>
+            </div>
+          </article>
+        ))}
+      </div>
+    </section>
   )
 }
 
---
+++ src/components/portfolio/Projects.js
@@ -0,0 +1,23 @@
+import POKEBALLIMG from "../../assets/pokeball.jpg";
+// Photo by <a href="https://unsplash.com/@shugo88?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Don H</a> on <a href="https://unsplash.com/s/photos/pokemon?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+import PINBALLIMG from "../../assets/pinball.jpg";
+// Photo by <a href="https://unsplash.com/ja/@lofiloup?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Louie Castro-Garcia</a> on <a href="https://unsplash.com/photos/UitmwXNMHiE?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+export const Projects = [
+  {
+    id: 0,
+    title: "Pokedex",
+    github: "https://github.com/seanrmcgovern/Pokedex",
+    demo: "https://www.youtube.com/watch?v=j3-sw69TbPo&t=86s",
+    image: POKEBALLIMG,
+  },
+  {
+    id: 1,
+    title: "Pinball Tracker",
+    github: "https://github.com/seanrmcgovern/Pinball-Tracker",
+    demo: "https://www.youtube.com/watch?v=A7WYX6Qy4VA&t=5s",
+    image: PINBALLIMG,
+  },
+];
+
+export default Projects;
