85f03c72a2bbf615aef6eb9007a1ea715886d163
Sean McGovern <srm5ka@virginia.edu>
2023-04-13 21:00:23 -0400

    Added footer section, updated socials and nav styles, and cleaned up project urls

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './footer.css'
+import { BsLinkedin, BsGithub } from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div style={{ height: 500 }}>Footer</div>
+    <footer>
+      <a href="#home" className='footer-logo'>Sean McGovern</a>
+
+      <ul className='footer-links'>
+        <li><a href='#home'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#skills'>Skills</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#services'>Services</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className="footer-socials">
+        <a href='https://linkedin.com/in/seanrmcgovern'><BsLinkedin /></a>
+        <a href='https://github.com/seanrmcgovern'><BsGithub /></a>
+      </div>
+      
+    </footer>
   )
 }
 
--- src/components/portfolio/Projects.js
+++ src/components/portfolio/Projects.js
@@ -8,14 +8,14 @@ export const Projects = [
     id: 0,
     title: "Pokedex",
     github: "https://github.com/seanrmcgovern/Pokedex",
-    demo: "https://www.youtube.com/watch?v=j3-sw69TbPo&t=86s",
+    demo: "https://www.youtube.com/watch?v=j3-sw69TbPo",
     image: POKEBALLIMG,
   },
   {
     id: 1,
     title: "Pinball Tracker",
     github: "https://github.com/seanrmcgovern/Pinball-Tracker",
-    demo: "https://www.youtube.com/watch?v=A7WYX6Qy4VA&t=5s",
+    demo: "https://www.youtube.com/watch?v=A7WYX6Qy4VA",
     image: PINBALLIMG,
   },
 ];
